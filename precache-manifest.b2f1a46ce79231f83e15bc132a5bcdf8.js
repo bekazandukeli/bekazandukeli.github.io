6cea2035af012a7b72650e63272cebefe0a96970
Beka Zandukeli <bekazandukeli@gmail.com>
2019-11-28 18:42:47 +0400

    Add files via upload

---
+++ precache-manifest.b2f1a46ce79231f83e15bc132a5bcdf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e392e62d4c09ba570b19454157370530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0c421409bcb64f49b6",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b0095564cb4b9ad7c2e7",
+    "url": "/static/css/main.65b6dd3d.chunk.css"
+  },
+  {
+    "revision": "4b0c421409bcb64f49b6",
+    "url": "/static/js/2.20774783.chunk.js"
+  },
+  {
+    "revision": "b0095564cb4b9ad7c2e7",
+    "url": "/static/js/main.33721285.chunk.js"
+  },
+  {
+    "revision": "7053a178898d5e374dff",
+    "url": "/static/js/runtime-main.181d5e0a.js"
+  }
+]);
